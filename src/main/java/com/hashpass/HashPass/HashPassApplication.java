3a556917abcd3cc7343fc428ac329f27bf57b0a1
tonycode2 <velurn123@gmail.com>
2025-07-21 13:15:39 -0600

    stat

---
+++ src/main/java/com/hashpass/HashPass/HashPassApplication.java
@@ -0,0 +1,13 @@
+package com.hashpass.HashPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashpass/HashPass/HashPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashpass.HashPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
