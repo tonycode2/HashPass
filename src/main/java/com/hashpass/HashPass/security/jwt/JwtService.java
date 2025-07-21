465eb7c2bb810cefc77c70e17849758c7290e004
tonycode2 <velurn123@gmail.com>
2025-07-21 14:52:59 -0600

    feat: Adding al JWT config

--- src/main/java/com/hashpass/HashPass/security/jwt/JwtService.java
+++ src/main/java/com/hashpass/HashPass/security/jwt/JwtService.java
@@ -1,10 +1,18 @@
 package com.hashpass.HashPass.security.jwt;
 
 import com.hashpass.HashPass.security.user.Users;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -16,6 +24,47 @@ public class JwtService {
     }
 
     private String getToken(HashMap<String, Object> extraClaims, Users user){
+        return Jwts.builder()
+                .claims(extraClaims)
+                .claim("userId", user.getId())
+                .claim("role", user.getRole())
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+1000+60+24))
+                .signWith(getKey())
+                .compact();
+    }
 
+    private SecretKey getKey(){
+        byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public getUsernameFromToken(String token){
+        return getClaim(token, Claims::getSubject);
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Date getExpirationDate(String token){
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return getExpirationDate(token).before(new Date());
     }
 }
